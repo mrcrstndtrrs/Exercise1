bedb32cd075a40ef883aadb8c855ed007cf4e3a4
mrcrstndtrrs <116280260+mrcrstndtrrs@users.noreply.github.com>
2023-02-06 16:55:18 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//Call all functions inside services and log updated value/s
+import { updateUser, getAllUsers } from "./services/users";
+
+console.log(getAllUsers());
+updateUser(1, { email: "roger@77.com", website: "hide-edit.org" });
+console.log(getAllUsers());
